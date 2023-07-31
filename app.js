b33205bafb6bca12d089c364c42176981b3c4598
Sanzhar Iden <sanzhar.iden@gmail.com>
2023-07-31 16:41:57 +0600

    second commit

--- app.js
+++ app.js
@@ -13,7 +13,8 @@ db.serialize(() => {
             name TEXT,
             surname TEXT,
             contacts TEXT,
-            date DATETIME
+            date DATETIME,
+            time INT
         )
     `);      
 });
@@ -23,44 +24,45 @@ app.use(cors())
 app.use(bodyParser.json({ type: 'application/json' }))
 
 app.post('/book', (req, res) => {
-    const { user, date } = req.body;
-    const { name, surname, IIN, contacts } = user;
-  
-    const selectRow = `SELECT * FROM clients WHERE IIN = ?`;
-  
-    db.get(selectRow, [IIN], (err, row) => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500);
+  const { user, date, timeInterval } = req.body; 
+  const { name, surname, IIN, contacts } = user;
+
+  const selectRow = `SELECT * FROM clients WHERE IIN = ?`;
+
+  db.get(selectRow, [IIN], (err, row) => {
+    if (err) {
+      console.error(err);
+      res.sendStatus(500);
+    } else {
+      if (row) {
+        const updateRow = `UPDATE clients SET date = ?, time = ? WHERE IIN = ?`;
+
+        db.run(updateRow, [date, timeInterval, IIN], (updateErr) => {
+          if (updateErr) {
+            console.error(updateErr);
+            res.sendStatus(500);
+          } else {
+            console.log(`User ${name} ${surname}'s date and time interval have been updated to ${date} in ${timeInterval}:00`);
+            res.sendStatus(200);
+          }
+        });
       } else {
-        if (row) {
-          const updateRow = `UPDATE clients SET date = ? WHERE IIN = ?`;
-  
-          db.run(updateRow, [date, IIN], (updateErr) => {
-            if (updateErr) {
-              console.error(updateErr);
-              res.sendStatus(500);
-            } else {
-              console.log(`User ${name} ${surname}'s date has been updated to ${date}`);
-              res.sendStatus(200);
-            }
-          });
-        } else {
-          const insertRow = `INSERT INTO clients (name, surname, IIN, contacts, date) VALUES (?, ?, ?, ?, ?)`;
-  
-          db.run(insertRow, [name, surname, IIN, contacts, date], (insertErr) => {
-            if (insertErr) {
-              console.error(insertErr);
-              res.sendStatus(404);
-            } else {
-              console.log(`User ${name} ${surname} booked for ${date}`);
-              res.sendStatus(200);
-            }
-          });
-        }
+        const insertRow = `INSERT INTO clients (name, surname, IIN, contacts, date, time) VALUES (?, ?, ?, ?, ?, ?)`; 
+
+        db.run(insertRow, [name, surname, IIN, contacts, date, timeInterval], (insertErr) => {
+          if (insertErr) {
+            console.error(insertErr);
+            res.sendStatus(404);
+          } else {
+            console.log(`User ${name} ${surname} booked for ${date} in ${timeInterval}:00`);
+            res.sendStatus(200);
+          }
+        });
       }
-    });
-});  
+    }
+  });
+});
+
 
 app.get('/check-iin', (req, res) => {
     const { iin } = req.query;
